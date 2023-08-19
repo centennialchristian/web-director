d32e0bcda5a76ecdae973cfc542301177eea7ab6
0byt3 <erolleman@outlook.com>
2023-08-19 11:10:46 -0700

    fixed install script

--- install-podman-compose.bash
+++ install-podman-compose.bash
@@ -7,12 +7,12 @@ elif which dnf 2>/dev/null; then
   dnf -y install epel-release
   dnf clean all
   dnf makecache
-  dnf -y podman podman-compose
+  dnf -y install podman podman-compose
 elif which yum 2>/dev/null; then
   yum -y install epel-release
   yum clean all
   yum makecache
-  yum -y podman podman-compose
+  yum -y install podman podman-compose
 else
   echo "Could not determine package manager" >&2
   exit 1
